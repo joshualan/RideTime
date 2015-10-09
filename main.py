572a8bb5fd44f6d1459bd44e3d199a8b72ce3f8f
joshualan <joshualan23@yahoo.com>
2015-10-09 00:39:33 -0400

    Test.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
 import urllib2
 
+from RequestMaker import makeRequest
+
 origin = "322.5 Western Ave, Cambridge MA"
 
 destination = "14 Oak Park Drive, Bedford MA"
@@ -10,7 +12,10 @@ url = origin + destination
 url = url.replace(" ", "+")
 url = url.replace(",", "")
 
-print url
+#print url
+
+makeRequest(origin, destination)
+
 
 #json.load(urllib2.urlopen("https://maps.googleapis.com/maps/api/distancematrix/json?origins=322.5+Western+Ave+Cambridge+MA&destinations=14+Oak+Park+Drive+Bedford+MA"))
-json.load(urllib2.urlopen(url))
+#json.load(urllib2.urlopen(url))
