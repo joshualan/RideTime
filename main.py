8dc33fffb6af7540725a8b2681b5b9701091cf6f
joshualan <joshualan23@yahoo.com>
2015-10-09 11:29:12 -0400

    Test.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-import urllib2
-
 from RequestMaker import makeRequest
+
+import pyttsx
 
 origin = "322.5 Western Ave, Cambridge MA"
 
@@ -12,10 +11,25 @@ url = origin + destination
 url = url.replace(" ", "+")
 url = url.replace(",", "")
 
-#print url
+data = makeRequest(origin, destination)
 
-makeRequest(origin, destination)
-
+print data['resourceSets'][0]['estimatedTotal']
 
 #json.load(urllib2.urlopen("https://maps.googleapis.com/maps/api/distancematrix/json?origins=322.5+Western+Ave+Cambridge+MA&destinations=14+Oak+Park+Drive+Bedford+MA"))
 #json.load(urllib2.urlopen(url))
+
+engine = pyttsx.init()
+engine.setProperty('rate', 70)
+
+voices = engine.getProperty('voices')
+for voice in voices:
+    print "Using voice:", repr(voice)
+    engine.setProperty('voice', voice.id)
+    engine.say("Hi there, how's you ?")
+    engine.say("A B C D E F G H I J K L M")
+    engine.say("N O P Q R S T U V W X Y Z")
+    engine.say("0 1 2 3 4 5 6 7 8 9")
+    engine.say("Sunday Monday Tuesday Wednesday Thursday Friday Saturday")
+    engine.say("Violet Indigo Blue Green Yellow Orange Red")
+    engine.say("Apple Banana Cherry Date Guava")
+engine.runAndWait()
