49d953464a36261a78c414525a43045e177a75d9
aestrada <joshualan23@yahoo.com>
2015-10-14 20:51:18 -0400

    Added some error handling, fixed up some naming.

--- requests.py
+++ requests.py
@@ -21,10 +21,28 @@ def makeRequest(origin , destination):
     destination = re.sub(rx, '+', destination)
 
     # Options are including traffic and to give 3 routes
+    # TODO: maybe give more options?
     flags = '&optmz=timeWithTraffic&maxSolns=3'
 
     url = ''.join(['http://dev.virtualearth.net/REST/V1/Routes?wp.0=',origin,flags,'&wp.1=',destination,'&key=',config.key])
 
-    response = urllib2.urlopen(url)
-    return json.load(response)
+    try:
+        response = urllib2.urlopen(url)
+    except urllib2.URLError, e:
+        print "There was an error in making the request: {}".format(e)
+        return None
 
+    j = json.load(response)
+
+    # Check to make sure that our credentials is good
+    if j['statusDescription'] != "OK":
+        print "The request was invalid."
+        return None
+
+    return j
+
+def parseRequest(j):
+    """
+    Takes json response from the Bing Routes API and returns a dictionary of relevant
+    information, e.g  route identifier, time took, mileage
+    """
