8fb3bfffb3804dc7315f474cd8d535c2ebe6165f
Alan Estrada <joshuaalan_Estrada@student.uml.edu>
2016-02-06 17:11:23 -0500

    Deleted a file and updated main..

--- main.py
+++ main.py
@@ -1,39 +1,47 @@
-from requests import makeRequest
-
+import bing
 import pyttsx
 import sys
 
-origin = "322.5 Western Ave, Cambridge MA"
 
-destination = "14 Oak Park Drive, Bedford MA"
+with open('addresses.txt', 'r') as f:
+    addresses = f.readlines()
 
-url = origin + destination
+origin = addresses[0]
+destination = addresses[1]
 
-url = url.replace(" ", "+")
-url = url.replace(",", "")
-
-request = makeRequest(origin, destination)
+request = bing.make_request(origin, destination)
 
 if request is None:
     sys.exit()
 
-print request['resourceSets'][0]['estimatedTotal']
+#print request['resourceSets'][0]['estimatedTotal']
 
-#json.load(urllib2.urlopen("https://maps.googleapis.com/maps/api/distancematrix/json?origins=322.5+Western+Ave+Cambridge+MA&destinations=14+Oak+Park+Drive+Bedford+MA"))
-#json.load(urllib2.urlopen(url))
+info = bing.parse_request(request)
 
-# engine = pyttsx.init()
-# engine.setProperty('rate', 70)
-#
-# voices = engine.getProperty('voices')
-# for voice in voices:
-#     print "Using voice:", repr(voice)
-#     engine.setProperty('voice', voice.id)
-#     engine.say("Hi there, how's you ?")
-#     engine.say("A B C D E F G H I J K L M")
-#     engine.say("N O P Q R S T U V W X Y Z")
-#     engine.say("0 1 2 3 4 5 6 7 8 9")
-#     engine.say("Sunday Monday Tuesday Wednesday Thursday Friday Saturday")
-#     engine.say("Violet Indigo Blue Green Yellow Orange Red")
-#     engine.say("Apple Banana Cherry Date Guava")
-# engine.runAndWait()
+print info
+
+engine = pyttsx.init()
+engine.setProperty('rate', 70)
+
+voices = engine.getProperty('voices')
+
+#for voice in voices:
+#    print "Using voice:", repr(voice)
+#    engine.setProperty('voice', voice.id)
+#    engine.say("Hi there, how's you ?")
+
+#    engine.say("Hi there Alan. I am Ride Time.")
+
+#    engine.say("Sunday Monday Tuesday Wednesday Thursday Friday Saturday")
+#    engine.say("Violet Indigo Blue Green Yellow Orange Red")
+#    engine.say("Apple Banana Cherry Date Guava")
+#engine.runAndWait()
+
+voices = engine.getProperty('voices')
+for voice in voices:
+    print "Using voice:", repr(voice)
+    print voice.age, voice.gender, voice.languages, voice.name
+
+#    engine.setProperty('voice', voice.id)
+#    engine.say("Hi there, how're you ?")
+#engine.runAndWait()
