90ff7b780c336c29e078b69808eb67e55a53f401
Alan Estrada <joshualan23@yahoo.com>
2016-02-10 00:15:44 -0500

    Solidified the protocol between the server and listener.

--- server.py
+++ server.py
@@ -55,6 +55,13 @@ def speak_confirmation():
     "Confirms that the app is in listening mode."
     speech.say('Ride Time is listening.')
 
+
+# Possible requests to server:
+# LISTEN - server goes into listening mode
+# STOP - quit out of listening mode without doing anything
+# REPEAT - ask the person to repeat themselves
+# TRAFFIC {name} - speaks traffic for a person
+# MESSAGES {name} - speaks out messages for a person
 def main():
     # Create an INET, STREAM-ing socket for our server
     try:
@@ -78,31 +85,60 @@ def main():
         c, addr = s.accept()
 
         data = c.recv(chunk_size)
-        
-        if data == "END":
-            c.send('SHUTDOWN')
+
+        # Sanity check
+        data = data.upper()
+
+        # Split the words in the request
+        request = data.split()
+
+        # Error if our request isn't length of 1 or 2
+        if len(request) != 1 or len(request) != 2:
+            c.send('BAD REQUEST')
             c.close()
-            break
+            continue
 
-
+        command = request[0]
+        name = request[1] if len(request) == 2 else None
         
+        if command == "LISTEN":
+            listening = True
+            c.send('WAITING')
+            c.close()
+        
+        if listening:
+            if command == "QUIT":
+                c.send('SHUTDOWN')
+                c.close()
+                break
 
+            # Don't do anything
+            if command == "STOP":
+                pass
+            elif command == "REPEAT":
+                # Say something like "I did not understand you, repeat?"
+                pass
+            elif command = "TRAFFIC":
+                names = ['ALAN', "ALLEN", 'COREY', 'PJ']
+                # This slice of code should be replaced by a cal to a DB
+                if name in names:
+                    print "Traffic for {}".format(name)
+                    speak_traffic(name)
+                else:
+                    # Say something here like "Dunno who the fuck that is"
+                    pass
+            elif command = "MESSAGES":
+                pass
+            # If we got here, we somehow got a bad request :(
+            else:
+                c.send('BAD REQUEST')
+                c.close()
+                continue
 
-        names = ['ALAN', "ALLEN", 'COREY', 'PJ']
-        l = [name for name in names if "TRAFFIC FOR {}".format(name) in data]
-        if len(l) == 1:
-            print "Traffic for {}".format(l[0])
-            speak_traffic(l[0])
-
-        elif len(l) > 1:
-            speech.say("Ambiguous request.")
-
-        elif data:
-            print "Server receives {} from {}:{}!".format(data, addr[0], addr[1])
-            # speak_traffic("ALAN")
-            
-        c.send('OK')
-        c.close()
+            # We're not listening anymore :)
+            listening = False
+            c.send('OK')
+            c.close()
 
     print "Server has had enough of yo shit" 
     s.close()
